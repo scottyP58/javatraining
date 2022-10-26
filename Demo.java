050e86997ece5bb770f8d7548fc3209c23575bd4
scottyP58 <smorrow44@gmail.com>
2022-10-26 14:07:05 -0500

    commit demo

---
+++ Demo.java
@@ -0,0 +1,34 @@
+public class Demo{
+
+	public static void main (String args[])
+
+	{
+
+		
+
+		long var1 = 2000;
+
+		double var2 = var1;
+
+		System.out.println(var1);
+
+		System.out.println(var2);
+
+		
+
+		long var3 = 2500;
+
+		double var4 = (long)var3;
+
+		System.out.println(var3);
+
+		System.out.println(var4);		
+
+		
+
+		
+
+	}
+
+}
+
